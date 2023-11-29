c394d06d17ad5fa0357a46eba5c98d926395bdf6
Mjkey2 <Mjmarentes2005@gmail.com>
2023-11-29 09:35:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,38 @@ def welcome():
 def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    
     sel = [Measurement.date,Measurement.prcp]
     queryresult = session.query(*sel).all()
+    #close
     session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    precipitaton_query_values = []
+    # Create for loop to iterate through query results
+    for prcp, date in precipitaton_query_values:
+        precipitation_dict = {}
+        #Create dictionary with key "precipitation" set to prcp from precipitation_query_results and key "date" to date from precipitation_query_results
+        precipitation_dict["precipitation"] = prcp
+        precipitation_dict["date"] = date
+        # Append values from precipitation_dict to your original empty list precipitation_query_values 
+        precipitaton_query_values.append(precipitation_dict)
+
+        #Return JSON format of your new list that now contains the dictionary of prcp and date values to your browser
+        return jsonify(precipitaton_query_values) 
+    
+# Create a route that returns a JSON list of stations from the database
+@app.route("/api/v1.0/stations")
+def station(): 
+    #create session
+    session = Session(engine)
+    """Return a list of stations from the database""" 
+    station_query_results = session.query(Station.station,Station.id).all()
+
+    session.close()  
+    stations_values = []
+    for station, id in station_query_results:
+        stations_values_dict = {}
+        stations_values_dict['station'] = station
+        stations_values_dict['id'] = id
+        stations_values.append(stations_values_dict)
+    return jsonify (stations_values) 
